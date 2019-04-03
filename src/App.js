9449691866f5fff0d3c75f47ccb7da5f71482d70
Bryan Hawkins <bhawkins3729@gmail.com>
2019-04-03 17:49:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./img/logodeckorator.png";
+import "./App.css";
+import Good from "./component/good.js";
+// import axios from "axios";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <body>
+        <div>
+          <header className="header">
+            <img src={logo} className="logo" alt="logo" />
+            <h1>Welcome to Deckorator</h1>
+          </header>
+
+          <a href="https://www.google.com">Google your deck</a>
+          <main>
+            <button>Liked this board</button>
+            <button>+</button>
+            <button>hated this board</button>
+
+            <Good />
+          </main>
+        </div>
+      </body>
     );
   }
 }
